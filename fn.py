e70e612af8cf34dc1f9154ad57b545bdeaa23447
jl.gzz <jl.gzz@outlook.com>
2021-10-28 20:15:23 -0500

    get data from api

--- fn.py
+++ fn.py
@@ -1,14 +1,40 @@
 import threading
+import requests
+
 
 periodo= 1.0
 
-def saludo():
-    threading.Timer(periodo,saludo).start()
-    print("hello world"+","+"be happy")
+"""
+def printIt(renglon:str="hola"):
+    threading.Timer(periodo,printIt).start()
+    print(renglon)"""
 
+def get_data(url:str,parametro:str):
+    #request object
+    r=requests.get(url)
 
+    if r.ok:
+        data=r.json()
+        #return(data)
+        valor=data[parametro]
+        return(valor)
+    else:
+        return(r.status_code)
 
-
-
+"""
 if  __name__=="__main__":
-    saludo()
+    get_data()  
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
